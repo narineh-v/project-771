96e119d3424aaf916e7ea190d87d8e78a8eea793
Narine Vardumyan <narinehvardumyan@gmail.com>
2018-03-05 13:39:37 +0300

    confl

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,10 +8,7 @@ import { createBrowserHistory as createHistory } from "history";
 import './App.scss';
 
 // Components
-// import Winter from '../components/Winter';
-//import Spring from '../components/Spring';
-// import Summer from '../components/Summer';
-// import Autumn from '../components/Autumn';
+
 import Seasons from './Seasons';
 
 
@@ -20,14 +17,7 @@ const history = createHistory(this.props);
 class App extends Component {
   state = {
     components: [
-<<<<<<< HEAD
-      {path: '/', name: Season},
-      {path: '/season/test', name: Spring}
-=======
       {path: '/', name: Seasons}
->>>>>>> e4d41733d9af305e0b2eea997603f4490d6f13db
-      // {path: '/summer', name: Summer},
-      // {path: '/autumn', name: Autumn}
     ]
   }
 
