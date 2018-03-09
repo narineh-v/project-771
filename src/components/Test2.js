d0f1c2c1a85e4ea5dc08718ce6b5cfbdc8da33d9
Narine Vardumyan <narinehvardumyan@gmail.com>
2018-03-09 17:10:36 +0300

    yurik pull

--- src/components/Test2.js
+++ src/components/Test2.js
@@ -16,7 +16,11 @@ class Test2 extends Component {
         songs: [],
         playing: true,
         width: window.innerWidth,
-        height: window.innerHeight
+        height: window.innerHeight,
+        x: 0,
+        y: 0,
+        zoom: 1,
+        scrollLeft: 0
     };
 
 
@@ -31,16 +35,38 @@ class Test2 extends Component {
       // });
     }
 
+    allowZoom(e) {
+      var el = document.getElementsByClassName('main-container')[0];
+      var coef = e.shiftKey || e.ctrlKey ? 0.5 : 2,
+          delm = document.documentElement,
+          oz = this.state.zoom,
+          nz = this.state.zoom * coef;
+
+          // update current
+          this.setState({
+            zoom: oz < 2 ? nz : 3,
+            x: `50%`,
+            y: 0
+          })
+
+          window.scrollTo(this.state.width/2, 0);
+
+      console.log('azz')
+    }
+
     componentWillMount() {
       //var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
       //var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
       () => this.updateDimensions();
+      (e) => this.allowZoom(e);
       //var W = window.innerWidth
-
+//window.scrollTo(500, 50);
     }
     componentDidMount() {
       window.addEventListener("resize", () => this.updateDimensions());
-      window.addEventListener("zoom", () => {console.log('yeah')});
+      window.addEventListener("dblclick", (e) => this.allowZoom(e));
+
+      window.scrollTo(500, 50);
         this._setInterval = setInterval(() => {
             let step = this.state.step;
             let classes = this.state.hs;
@@ -100,6 +126,8 @@ class Test2 extends Component {
       // [...foregroundImage].map((el,i) => {
       //   el.width = this.state.width;
       // });
+
+
         return (
             <div>
                 {this.state.seasons.map((season, index) => {
@@ -107,13 +135,25 @@ class Test2 extends Component {
                     backgroundImage: `url(${require(`../assets/img/${season}/foreground.png`)})`
                   }
 
+                  // let zoomStyles = {
+                  //   transform: 'translate('+ {this.state.x} +'px, '+ {this.state.y} +'px) '
+                  //   + 'scale('+ {this.state.nz}+')'
+                  // };
+
+                  let zoomStyles = {
+                    transform: `translate(${this.state.x}, 0) scale(${this.state.zoom})`
+                  };
+
+
+
+
                     return (
 
                             <div key={index} className={['wrapper' , season , this.state.hs[index] ].join(' ')}>
 
                                 {/*----------- Content -----------*/}
                                 <div className="content">
-                                    <div className="main-container"  >
+                                    <div className="main-container" style = {zoomStyles}>
                                         <img src={require(`../assets/img/${season}/foreground.png`)}
                                              className="foreground-image"
                                              alt="foreground"
@@ -132,9 +172,7 @@ class Test2 extends Component {
                                         </div>
                                         {/*----------- Boy End-----------*/}
 
-                                        <div rel="" className="plus-minus-icon" onClick={this.togglePlay}>
-                                            <img src={require('../assets/img/icons/plus-minus-icon.png')} alt="plus-minus-icon"/>
-                                        </div>
+                                        <img src={require('../assets/img/icons/plus-minus-icon.png')} className = "plus-minus-icon" alt="plus-minus-icon" onClick={this.togglePlay}/>
                                     </div>
                                 </div>
                                 {/*----------- Content End -----------*/}
@@ -142,7 +180,7 @@ class Test2 extends Component {
 
                     );
                 })}
-                <audio className = "audioTag" autoPlay loop>
+                <audio className = "audioTag" loop>
                     <source src={require("../assets/sounds/ocean_sound.mp3")} type=""/>
                     Your browser does not support the audio element.
                 </audio>
